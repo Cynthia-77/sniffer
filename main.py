625f6e5b0030071d9f874fc526b62d2b380fa2af
uniqueR <1321760271@qq.com>
2023-03-18 23:28:35 +0800

    controller: init and set connection

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from PyQt5 import QtWidgets
 
 import snifferGUI
-import snifferGUIController
+import snifferController
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
--- snifferController.py
+++ snifferController.py
@@ -4,14 +4,8 @@ class SnifferController:
         self.sniffer = None
 
     def set_connection(self):
-
         self.ui.startButton.clicked.connect(self.start)
         self.ui.pauseButton.clicked.connect(self.pause)
-        self.ui.resetButton.clicked.connect(self.ui.reset)
-
-        self.ui.buttonFilter.clicked.connect(self.Filter)
-        self.ui.tableWidget.itemClicked.connect(self.ui.showItemDetail)
-        self.ui.buttonPostFilter.clicked.connect(self.PostFilter)
-        self.ui.tableWidget.customContextMenuRequested.connect(self.ui.showContextMenu)
-        self.ui.TraceAction.triggered.connect(self.Trace)
-        self.ui.saveAction.triggered.connect(self.Save)
+        self.ui.resetButton.clicked.connect(self.reset)
+        self.ui.protocolFilterAfterCapture.activated.connect(self.filter)
+        self.ui.tableWidget.itemClicked.connect(self.show_item_detail)
